e2ec41a17641fa25929728df4e1e848a6e637cc3
N4v1ds0n <damian.droste@gmx.de>
2025-04-26 00:03:43 +0200

    Add score count functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,14 +89,16 @@ function calculateCorrectAnswer () {
  * Bumps up the score count if answer was correct
  */
 function incrementScore() {
-    parseInt(document.getElementById('score').value)++;
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore
 }
 
 /**
  * Increases cont fr incorrect guesses if answer is wrong
  */
 function incrementWrongAnswer() {
-    parseInt(document.getElementById('incorrect').value)++;
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore
 }
 
 function displayAdditionQuestion(operand1, operand2) {
